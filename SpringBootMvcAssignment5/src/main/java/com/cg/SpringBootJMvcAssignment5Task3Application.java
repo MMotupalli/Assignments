3abd4a6e4f6d1ea01bfbe8e37e8b678ca22e9198
MMotupalli <49578157+MMotupalli@users.noreply.github.com>
2020-04-17 10:31:51 +0530

    Add files via upload

---
+++ SpringBootMvcAssignment5/src/main/java/com/cg/SpringBootJMvcAssignment5Task3Application.java
@@ -0,0 +1,13 @@
+package com.cg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJMvcAssignment5Task3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJMvcAssignment5Task3Application.class, args);
+	}
+
+}
